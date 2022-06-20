4c8fed2d6492bc99f4ef895ef72038d0d11f684f
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-06-20 20:51:48 +0800

    Update two_sum.py

--- Problems/No.1_Two_Sum/two_sum.py
+++ Problems/No.1_Two_Sum/two_sum.py
@@ -5,20 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
-        # if first two sums are the solution
-        if (nums[0]+nums[1] == target):
-            return [0, 1]
-        else:
-            find = dict({nums[0]: 0, nums[1]: 1})   # dictionary which save the elements before index i
-            loop_size = len(nums)                   # set the loop size
-            
-            # iterate the whole list once
-            for i in range(2, loop_size):
-                find_num = target-nums[i]   
-                
-                # if find out the solution then return it, else add element of index i into find
-                if find_num in find:    
-                    return [find[find_num], i]
-                else:                 
-                    find[nums[i]] = i
+       
+        find = {}   # dictionary which save the elements before index i
+        # iterate the whole list once
+        for i in range(len(nums)):
+            find_num = target-nums[i]   
+
+            # if find out the solution then return it, else add element of index i into find
+            if find_num in find:    
+                return [find[find_num], i]
+            else:                 
+                find[nums[i]] = i
