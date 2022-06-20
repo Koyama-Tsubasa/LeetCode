374b18663dca45cfd0faeaf89a011c9969cdd564
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-06-20 15:50:52 +0800

    Update two_sum.py

--- Problems/No.1_Two_Sum/two_sum.py
+++ Problems/No.1_Two_Sum/two_sum.py
@@ -10,11 +10,11 @@ class Solution(object):
         if (nums[0]+nums[1] == target):
             return [0, 1]
         else:
-            find = [nums[0], nums[1]]   # list which save the elements before index i
+            find = dict({nums[0]: 0, nums[1]: 1})   # dictionary which save the elements before index i
             loop_size = len(nums)       # set the loop size
             for i in range(2, loop_size):
                 find_num = target-nums[i]   
                 if find_num in find:    # find out the solution then return it
-                    return [find.index(find_num), i]
+                    return [find[find_num], i]
                 else:                   # add element of index i into find
-                    find.append(nums[i])
+                    find[nums[i]] = i
