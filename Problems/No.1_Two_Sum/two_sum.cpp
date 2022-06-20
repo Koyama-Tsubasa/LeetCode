b2cee3998e1bf756f5d73ea10430a66a96191643
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-06-20 11:26:57 +0800

    Update two_sum.cpp

--- Problems/No.1_Two_Sum/two_sum.cpp
+++ Problems/No.1_Two_Sum/two_sum.cpp
@@ -10,12 +10,15 @@ public:
         else {
             int i;
             vector<int> vector_find = {nums[0], nums[1]};   // create a vector to save elements before index i
-            vector<int>::iterator it;   // an iterator to find element in vector_find
+            vector<int>::iterator it;                       // an iterator to find element in vector_find
+            
+            // loop from index 2 to the end
             for (i=2; i<nums.size(); i++) {
                 it = find(vector_find.begin(), vector_find.end(), target-nums[i]);
-                if (it != vector_find.end()) break;
+                if (it != vector_find.end()) break;     // find out the solution then break
                 else vector_find.push_back(nums[i]);    // add element of index i into vector_find
             }
+            
             int find_solution = distance(vector_find.begin(), it);  // calculate the distance between index 0 and it
             vector<int> solution = {i, find_solution};
             return solution;
