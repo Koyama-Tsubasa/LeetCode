b9800c99d3a02862d699645e7b04d5033758b629
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-06-22 09:03:59 +0800

    Create roman_to_integer.py

---
+++ Problems/No.13_Roman_to_Integer/roman_to_integer.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        # construct Roman table
+        Roman_table = {
+            "I": 1, 
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        cur = 0     # save current partial Roman integer
+        pre = 0     # save previous partial Roman integer
+        output = 0  # calculate the output
+        
+        # start compute
+        for sub_s in s:
+            cur = Roman_table[sub_s]
+            
+            # check whether the subtraction is used or not
+            if pre < cur:
+                output = output - 2*pre + cur
+            else:
+                output = output + cur
+            pre = cur
+        
+        return output
+        
+        
