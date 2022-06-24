31f3600adcf434d34bc23d97249ea3fdc4cb33f7
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-06-24 13:17:24 +0800

    Create valid_parentheses.cpp

---
+++ Problems/No.20_Valid_Parentheses/valid_parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> STACK;  // the stack to save (, [, {
+        char check;         // to check the top of the stack
+        
+        // start compute
+        for (int i=0; i<s.length(); i++) {
+            
+            // check whether to push into the stack or pop out
+            if (s[i] == '(' or s[i] == '[' or s[i] == '{') STACK.push(s[i]);
+            else {
+                
+                // check if the stack is empty while you are going to pop out
+                if (STACK.empty()) return false;
+                else {
+                    
+                    // check whether it is valid or not
+                    check = STACK.top();
+                    if ( not(s[i] == ')' and check == '(') and
+                         not(s[i] == ']' and check == '[') and
+                         not(s[i] == '}' and check == '{')) return false;
+                    else STACK.pop();
+                    
+                }
+                
+            }
+            
+        }
+        
+        // check if the stack is empty when you finish iterating s
+        if (STACK.empty()) return true;
+        else return false;
+        
+    }
+};
