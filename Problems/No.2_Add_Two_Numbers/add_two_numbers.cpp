8396d9608b0d8f48dc9a2e101c4850702597e205
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-06-27 16:10:10 +0800

    Update add_two_numbers.cpp

--- Problems/No.2_Add_Two_Numbers/add_two_numbers.cpp
+++ Problems/No.2_Add_Two_Numbers/add_two_numbers.cpp
@@ -13,44 +13,28 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
         // create the parameter to store partial sum and carry
-        int partial_sum = 0;
+        int partial_sum;
         int carry = 0;
-      
+        
         // create the list of answer
-        ListNode* l_ans = new ListNode(0); 
+        ListNode* l_ans = new ListNode(0);
         ListNode* ans = l_ans;
         
-        // add two numbers until one becomes null
-        while(l1 and l2) {
-            
-            // calculate partial sum and carry
-            partial_sum = l1->val + l2->val + carry;
-            carry = partial_sum/10;
-            partial_sum = partial_sum%10;
-            
-            // create new node with partial_sum/10
-            ListNode* new_node = new ListNode(partial_sum);
-            l_ans->next = new_node;
-            
-            l1 = l1->next;
-            l2 = l2->next;
-            l_ans = l_ans->next;
-            
-        }
         
-        // check if l1 and l2 are all visited
-        while(l1 or l2) {
+        // start compute
+        while(l1 or l2 or carry) {
             
             // calculate partial sum and carry
+            partial_sum = carry;
             if (l1) {
                 
-                partial_sum = l1->val + carry;
+                partial_sum += l1->val;
                 l1 = l1->next;
                 
             }
-            else {
+            if (l2) {
                 
-                partial_sum = l2->val + carry;
+                partial_sum += l2->val;
                 l2 = l2->next;
                 
             }
@@ -61,14 +45,6 @@ public:
             ListNode* new_node = new ListNode(partial_sum);
             l_ans->next = new_node;
             l_ans = l_ans->next;
-            
-        }
-        
-        // check if the last carry is 1
-        if (carry) {
-            
-            ListNode* new_node = new ListNode(carry);
-            l_ans->next = new_node;
             
         }
         
