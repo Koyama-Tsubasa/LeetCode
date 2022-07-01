9b368a81738b9d657adc9bd475f08d76d11d6baa
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-07-01 10:29:22 +0800

    Create reverse_integer.cpp

---
+++ Problems/No.7_Reverse_Integer/reverse_integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int RI_ans = 0;
+        int temp;
+        
+        // start compute
+        while (x != 0) {
+
+            temp = x%10;
+            
+            // check whether the value overflow
+            if (((RI_ans > 214748364) or 
+                 ((RI_ans == 214748364) and (temp >= 7))) or
+                ((RI_ans < -214748364) or 
+                 ((RI_ans == -214748364) and (temp >= 8)))) return 0;
+            else RI_ans = RI_ans*10 + temp;
+            
+            x /= 10;
+
+        } 
+        
+        return RI_ans;
+        
+    }
+};
