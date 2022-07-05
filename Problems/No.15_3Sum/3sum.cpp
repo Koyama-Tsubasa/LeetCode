572895ed4cb18f8b39bd3458d212c53d8981ad01
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-07-05 15:02:05 +0800

    Create 3sum.cpp

---
+++ Problems/No.15_3Sum/3sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        // store the pointers
+        int zero, l_index, r_index;
+        vector<vector<int>> threesum_ans;
+        
+        // preprocess
+        if (nums.size() < 3) return threesum_ans;
+        
+        // start compute
+        sort(nums.begin(), nums.end());
+        for (int i=0; i<nums.size()-2; i++) {
+            
+            // check if it violate the rules
+            if (nums[i] > 0) break;
+            else if (i == 0 or (i > 0 and (nums[i] != nums[i-1]))) {
+                
+                zero = -nums[i];
+                l_index = i + 1;
+                r_index = nums.size() - 1;
+
+                while (l_index < r_index) {
+
+                    if (nums[l_index] + nums[r_index] == zero) {
+
+                        threesum_ans.push_back({nums[i], nums[l_index], nums[r_index]});
+                        while ((l_index < r_index) && (nums[l_index] == nums[l_index+1])) l_index++;
+                        while ((l_index < r_index) && (nums[r_index-1] == nums[r_index])) r_index--;
+                        l_index++;
+                        r_index--;
+
+                    }
+                    else if (nums[l_index] + nums[r_index] < zero) l_index++;
+                    else r_index--;
+                
+                }
+                
+            }
+            
+        }
+        
+        return threesum_ans;
+        
+    }
+};
