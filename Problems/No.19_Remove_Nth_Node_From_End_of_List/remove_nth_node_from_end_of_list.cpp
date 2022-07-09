ea1f8c88ee3c0b2394e85f6e815e0fb88bd10b82
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-07-09 10:39:53 +0800

    Create remove_nth_node_from_end_of_list.cpp

---
+++ Problems/No.19_Remove_Nth_Node_From_End_of_List/remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        // store some parameters
+        int sz = 1;
+        ListNode* curr = head;
+        
+        // preprocess (calculate the total size)
+        while (curr->next != NULL) {
+            
+            curr = curr->next;
+            sz++;
+            
+        }
+        
+        // edge case
+        if (sz == 1) return NULL;
+        else if (sz == n) return head->next;
+        
+        // start compute
+        curr = head;
+        for (int i=0; i<sz-n-1; i++) curr = curr->next;
+        curr->next = curr->next->next;
+        
+        return head;
+        
+    }
+};
