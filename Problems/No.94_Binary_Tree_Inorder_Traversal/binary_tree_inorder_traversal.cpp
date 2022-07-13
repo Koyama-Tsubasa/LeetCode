496127597cb9720490a0a0605e60779f0b1cac95
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-07-13 21:46:58 +0800

    Create binary_tree_inorder_traversal.cpp

---
+++ Problems/No.94_Binary_Tree_Inorder_Traversal/binary_tree_inorder_traversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    // Inorder Tree Traversal
+    void BTIT(TreeNode* node, vector<int> &ans) {
+        
+        if (node == NULL) return;
+        else {
+            
+            BTIT(node->left, ans);
+            ans.push_back(node->val);
+            BTIT(node->right, ans);
+            
+        }
+        
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        // parameters
+        vector<int> BTIT_ans;
+        
+        // start compute
+        if (root == NULL) return BTIT_ans;
+        else {
+            
+            BTIT(root->left, BTIT_ans);
+            BTIT_ans.push_back(root->val);
+            BTIT(root->right, BTIT_ans);
+            
+        }
+        
+        return BTIT_ans;
+        
+    }
+};
