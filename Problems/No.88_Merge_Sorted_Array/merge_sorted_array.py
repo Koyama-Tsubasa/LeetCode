7c17f29c9c1d7dad78d2576a5ba138a3f2e86114
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-07-13 14:53:38 +0800

    Create merge_sorted_array.py

---
+++ Problems/No.88_Merge_Sorted_Array/merge_sorted_array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        
+        # start compute
+        for i in range(n):
+            nums1.remove(nums1[-1])
+        nums1.extend(nums2)
+        nums1.sort()
+        
