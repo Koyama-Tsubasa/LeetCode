a526920b4794fb2469f4aba1979a7c3d258db475
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-07-20 08:42:37 +0800

    Update minimum_depth_of_binary_tree.cpp

--- Problems/No.111_Minimum_Depth_of_Binary_Tree/minimum_depth_of_binary_tree.cpp
+++ Problems/No.111_Minimum_Depth_of_Binary_Tree/minimum_depth_of_binary_tree.cpp
@@ -14,15 +14,38 @@ public:
     int minDepth(TreeNode* root) {
         
         // parameters
-        int left, right;
+        queue<TreeNode*> Q;
+        TreeNode *temp;
+        int depth = 0, Q_size;
         
-        // start compute
+        // start compute, using BFS method
         if (root == NULL) return 0;
         else {
             
-            left = minDepth(root->left);
-            right = minDepth(root->right);
-            return (!min(left, right)) ? max(left, right)+1 : min(left, right)+1;
+            Q.emplace(root);
+            while (Q.size()) {
+                
+                Q_size = Q.size();
+                for (int i=0; i<Q_size; i++) {
+                    
+                    temp = Q.front();
+                    Q.pop();
+                    
+                    if (temp->left==NULL and temp->right==NULL) return depth + 1;
+                    else{
+                        
+                        if (temp->left) Q.emplace(temp->left);
+                        if (temp->right) Q.emplace(temp->right);
+                        
+                    }
+                    
+                }    
+                
+                depth++;
+                
+            }
+            
+            return depth;
             
         }
         
