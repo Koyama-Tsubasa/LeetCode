8c2fb03ac3e3147d1d180bed9f819c15f1571b55
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-07-21 15:44:12 +0800

    Update path_sum.cpp

--- Problems/No.112_Path_Sum/path_sum.cpp
+++ Problems/No.112_Path_Sum/path_sum.cpp
@@ -13,10 +13,6 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         
-        // parameter
-        int next_tS;
-        
-        // start compute
         if (root == NULL) return false;
         else if (root->left==NULL and root->right==NULL) return targetSum==root->val;
         else return hasPathSum(root->left, targetSum-root->val) or hasPathSum(root->right, targetSum-root->val);
