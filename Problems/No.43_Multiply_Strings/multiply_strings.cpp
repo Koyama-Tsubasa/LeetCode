e38e3d5f30de2aaa8785c72ab8c60dacea032876
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-08-18 13:45:26 +0800

    Update multiply_strings.cpp

--- Problems/No.43_Multiply_Strings/multiply_strings.cpp
+++ Problems/No.43_Multiply_Strings/multiply_strings.cpp
@@ -10,20 +10,17 @@ public:
         
         // start compute
         if ((num1_len==1 and num1[0]=='0') or (num2_len==1 and num2[0]=='0')) return "0";
-        else {
             
-            for (int i=0; i<num1_len+num2_len; i++) ans += "0";
-            for (int i=num1_len-1; i>=0; i--) {
-                for (int j=num2_len-1; j>=0; j--) {
-                    
-                    product = (num1[i]-'0')*(num2[j]-'0');
-                    product += ans[i+j+1]-'0';
-                    ans[i+j+1] = product%10 + '0';
-                    ans[i+j] += product/10;
-                    
-                }
+        for (int i=0; i<num1_len+num2_len; i++) ans += "0";
+        for (int i=num1_len-1; i>=0; i--) {
+            for (int j=num2_len-1; j>=0; j--) {
+
+                product = (num1[i]-'0')*(num2[j]-'0');
+                product += ans[i+j+1]-'0';
+                ans[i+j+1] = product%10 + '0';
+                ans[i+j] = ans[i+j] + product/10;
+
             }
-            
         }
         
         for (int i=0; i<num1_len+num2_len; i++) if (ans[i]!='0') return ans.substr(i);
