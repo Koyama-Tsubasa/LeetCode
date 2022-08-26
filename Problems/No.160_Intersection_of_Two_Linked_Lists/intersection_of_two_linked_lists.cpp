6ca2fb7969420ad32d81418dc3d03c6bfac92046
K.Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2022-08-26 15:22:28 +0800

    Create intersection_of_two_linked_lists.cpp

---
+++ Problems/No.160_Intersection_of_Two_Linked_Lists/intersection_of_two_linked_lists.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    // count the number of nodes
+    int CountNodes(ListNode *node) {
+        
+        int count = 0;
+        while (node->next) {
+            
+            count++;
+            node = node->next;
+            
+        }
+        
+        return count;
+        
+    }
+    
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+     
+        // parameters
+        int A_num = CountNodes(headA);
+        int B_num = CountNodes(headB);
+        ListNode *a = headA, *b = headB;
+        
+        // preprocess to match the list length
+        for (int i=0; i<A_num-B_num; i++) a = a->next;
+        for (int i=0; i<B_num-A_num; i++) b = b->next;
+        
+        // start checking
+        while (a != b) {
+            
+            a = a->next;
+            b = b->next;
+            
+        }
+        
+        return a;
+        
+    }
+};
