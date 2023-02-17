a6886f3ef9a0d0cc85a2526077086b7ead2957cb
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-02-17 14:05:07 +0800

    Update number_of_1_bits.cpp

--- Problems/No.191_Number_of_1_Bits/number_of_1_bits.cpp
+++ Problems/No.191_Number_of_1_Bits/number_of_1_bits.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         
+        // convert uint32_t into bitset & count the number of 1s
         std::bitset<32> bit_string(n);
         return bit_string.count();
 
