19a5b0435c83f0635b2e300e09d89cf5988dd321
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-02-19 20:24:50 +0800

    Create reverse_linked_list_recursive.py

---
+++ Problems/No.206_Reverse_Linked_List/reverse_linked_list_recursive.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        # start computing
+        if ((head == None) or (head.next == None)):
+            return head
+        reversed_list = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return reversed_list
