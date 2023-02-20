5746a2537e516655cef611c26d5e8342a60643a4
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-02-20 17:14:38 +0800

    Create first_missing_positive.cpp

---
+++ Problems/No.41_First_Missing_Positive/first_missing_positive.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        // parameters
+        int i = 0;
+        int sizeN = nums.size();
+
+        // start computing
+        while (i < sizeN) {
+
+            if ((nums[i] > 0) && (nums[i] <= sizeN) && 
+                (nums[nums[i]-1] != nums[i])) std::swap(nums[nums[i]-1], nums[i]);
+            else i++;
+
+        }
+
+        for (int i=0; i<sizeN; i++) if (nums[i] != i+1) return i+1;
+        return sizeN+1;
+
+    }
+};
