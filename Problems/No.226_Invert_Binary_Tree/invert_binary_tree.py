bb9713658e91fbc34bc15d1e5901a8096836c128
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-02-22 11:18:45 +0800

    Create invert_binary_tree.py

---
+++ Problems/No.226_Invert_Binary_Tree/invert_binary_tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+
+        # parameter
+        temp = None
+
+        # start computing
+        if not root:
+            return None
+        
+        temp = root.left
+        root.left = self.invertTree(root.right)
+        root.right = self.invertTree(temp)
+
+        return root
