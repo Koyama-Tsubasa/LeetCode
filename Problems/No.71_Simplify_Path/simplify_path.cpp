30e8670f20fb983770ead8f77a8dd67e9b03c935
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-02-25 13:48:52 +0800

    Create simplify_path.cpp

---
+++ Problems/No.71_Simplify_Path/simplify_path.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        // parameters
+        std::stack<std::string> relative_paths;
+        std::stringstream ss(path);
+        std::string temp, simplifiedPath = "";
+
+        // start computing
+
+        // split the path into relative paths
+        while (getline(ss, temp, '/')) 
+            if (temp != "") {
+
+                if (temp == ".") continue;
+                else if (temp == "..") { if (!relative_paths.empty()) relative_paths.pop(); }
+                else relative_paths.push(temp);
+
+            }
+        
+        // merge relative paths
+        while (!relative_paths.empty()) {
+
+            simplifiedPath = "/" + relative_paths.top() + simplifiedPath;
+            relative_paths.pop();
+
+        }
+
+        if (simplifiedPath == "") return "/";
+        return simplifiedPath;
+        
+    }
+};
