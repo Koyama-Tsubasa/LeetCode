5152cd1f630c192efbe0d7f52b1058cbb48664fa
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-02-26 15:09:32 +0800

    Create set_matrix_zeros.cpp

---
+++ Problems/No.73_Set_Matrix_Zeros/set_matrix_zeros.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        // parameters
+        int rowS = matrix.size();
+        int colS = matrix[0].size();
+        bool rFlag = false, cFlag = false;
+
+        // start computing
+
+        // search the first row & the first column
+        for (int i=0; i<colS; i++) if (matrix[0][i] == 0) rFlag = true;
+        for (int i=0; i<rowS; i++) if (matrix[i][0] == 0) cFlag = true;
+
+        // search for others and convert the corresponding element in the 1st row&column to 0
+        for (int i=0; i<rowS; i++)
+            for (int j=1; j<colS; j++)
+                if (matrix[i][j] == 0) 
+                    matrix[i][0] = matrix[0][j] = 0;
+
+        // update the zeros correspond to the inside area
+        for (int i=1; i<rowS; i++)
+            for (int j=1; j<colS; j++)
+                if ((matrix[i][0] == 0) || (matrix[0][j] == 0))
+                    matrix[i][j] = 0;
+
+        // update the the zeros correspond to the outside area
+        if (rFlag == true) for (int i=0; i<colS; i++) matrix[0][i] = 0;
+        if (cFlag == true) for (int i=0; i<rowS; i++) matrix[i][0] = 0;
+
+    }
+};
