051e212eed5c8f6f7c2a4d6f328ce169ad16abb3
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-02-28 09:30:14 +0800

    Create sort_colors.cpp

---
+++ Problems/No.75_Sort_Colors/sort_colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        // parameters
+        int cur = 0;
+        int left = 0;
+        int right = nums.size() - 1;
+
+        // start computing
+        while (cur <= right) 
+            switch(nums[cur]) {
+
+                case 0:
+                    std::swap(nums[cur], nums[left]);
+                    left++;
+                    cur++;
+                    break;
+                case 1:
+                    cur++;
+                    break;
+                case 2:
+                    std::swap(nums[cur], nums[right]);
+                    right--;
+                    break;
+                default:
+                    break;
+
+            }
+
+    }
+};
