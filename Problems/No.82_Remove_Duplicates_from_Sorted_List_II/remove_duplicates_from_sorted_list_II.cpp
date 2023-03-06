80df4958563d344c3be5ab21c344c9691026b32d
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-06 14:36:29 +0800

    Create remove_duplicates_from_sorted_list_II.cpp

---
+++ Problems/No.82_Remove_Duplicates_from_Sorted_List_II/remove_duplicates_from_sorted_list_II.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        // parameters
+        ListNode *newHead = new ListNode(0, head);
+        ListNode *dFirst = newHead, *dLast;
+        bool NULL_check;
+
+        // start computing
+        if ((head == NULL) || (head->next == NULL)) return head;
+        dLast = dFirst->next->next;
+        while (dLast != NULL) {
+
+            NULL_check = false;
+            if (dFirst->next->val == dLast->val) {
+
+                while (dFirst->next->val == dLast->val) {
+
+                    dLast = dLast->next;
+                    if (dLast == NULL) {
+
+                        NULL_check = true;
+                        break;
+
+                    }
+
+                }
+                dFirst->next = dLast;
+
+            }
+            else dFirst = dFirst->next;
+
+            if (!NULL_check) dLast = dLast->next;
+
+        }
+
+        return newHead->next;
+
+    }
+};
