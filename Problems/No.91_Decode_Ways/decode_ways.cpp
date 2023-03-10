cc633621678ddadcf5b90afc4896b3b06c8e76c4
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-10 17:11:18 +0800

    Create decode_ways.cpp

---
+++ Problems/No.91_Decode_Ways/decode_ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        
+        // parameters
+        int s_length = s.length();
+        std::vector<int> DP(s_length+1, 0);
+
+        // start computing
+        DP[0] = 1;
+        for (int i=1; i<=s_length; i++) {
+
+            if (s[i-1] != '0') DP[i] += DP[i-1];
+
+            if ((1 < i) && ((s[i-2] == '1') || ((s[i-2] == '2') && (s[i-1] <= '6')))) 
+                DP[i] += DP[i-2];
+
+        }
+        
+        return DP[s_length];
+
+    }
+};
