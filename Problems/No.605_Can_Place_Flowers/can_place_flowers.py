5b71e8c45f16be019aa2824dbe2ab489ae532b2b
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-14 16:55:14 +0800

    Update can_place_flowers.py

--- Problems/No.605_Can_Place_Flowers/can_place_flowers.py
+++ Problems/No.605_Can_Place_Flowers/can_place_flowers.py
@@ -12,8 +12,8 @@ class Solution(object):
         else:
             for i in range(len(flowerbed)):
                 if (not flowerbed[i] and
-                    ((i == 0) or (flowerbed[i-1] == 0)) and
-                    ((i == len(flowerbed)-1) or (flowerbed[i+1] == 0))):
+                    ((i == 0) or not flowerbed[i-1]) and
+                    ((i == len(flowerbed)-1) or not flowerbed[i+1])):
                     n -= 1
                     if (n == 0):
                         return True
