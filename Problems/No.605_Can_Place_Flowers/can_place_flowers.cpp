7842b74ce6464d868ef2cf215440651327db4036
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-14 16:49:43 +0800

    Update can_place_flowers.cpp

--- Problems/No.605_Can_Place_Flowers/can_place_flowers.cpp
+++ Problems/No.605_Can_Place_Flowers/can_place_flowers.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        
-        // parameter
-        bool planted;
-        int FbedSize = flowerbed.size();
 
         // start computing
         if (n == 0) return true;
         else {
 
-            for (int i=0; i<FbedSize; i++) {
+            for (int i=0; i<flowerbed.size(); i++) {
                 
                 if (!flowerbed[i] && 
                     (((i == 0) || !flowerbed[i-1]) && 
-                    ((i == FbedSize-1) || !flowerbed[i+1]))) {
+                    ((i == flowerbed.size()-1) || !flowerbed[i+1]))) {
                     
                     flowerbed[i] = 1;
                     n--;
