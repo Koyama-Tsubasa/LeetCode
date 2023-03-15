b9d14ae6912eab36bc94c46e6ab6dc2e6777ca11
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-15 16:19:16 +0800

    Create max_increase_to_keep_city_skyline.cpp

---
+++ Problems/No.807_Max_Increase_to_Keep_City_Skyline/max_increase_to_keep_city_skyline.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        
+        // parameter
+        int increased = 0;
+        std::vector<int> max_col(grid[0].size(), 0), max_row(grid.size(), 0);
+
+        // start computing
+
+        // find the maximum of each column
+        for (int col=0; col<grid[0].size(); col++)
+            for (int row=0; row<grid.size(); row++)
+                max_col[col] = std::max(max_col[col], grid[row][col]);
+
+        // find the maximum of each row & start increasing
+        for (int row=0; row<grid.size(); row++) {
+
+            max_row[row] = *std::max_element(grid[row].begin(), grid[row].end());
+            for (int col=0; col<grid[0].size(); col++) 
+                increased += (std::min(max_row[row], max_col[col]) - grid[row][col]);
+        
+        }
+
+
+        return increased;
+
+    }
+};
