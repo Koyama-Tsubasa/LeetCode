b9ffa52d4c08a403802f0998e19dc103b3179b82
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-16 21:00:45 +0800

    Create append_k_integers_with_minimal_sum.cpp

---
+++ Problems/No.2195_Append_K_Integers_With_Minimal_Sum/append_k_integers_with_minimal_sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long long minimalKSum(vector<int>& nums, int k) {
+        
+        // parameter
+        long long appended_sum = 0;
+        int added_num = 0;
+        int i = 1;
+
+        // start computing
+        sort(nums.begin(), nums.end());
+        nums.insert(nums.begin(), 0);
+        
+        while (k > 0) {
+
+            // the case between two nums
+            added_num = nums[i] - nums[i-1] - 1;
+            if (added_num > 0) {
+                
+                appended_sum += ((nums[i-1]+1) + (nums[i-1]+std::min(added_num, k)))*(long)std::min(added_num, k)/2;
+                k -= added_num;cout<<k<<endl;
+
+            }
+            i++;
+
+            // the case after all nums
+            if ((i >= nums.size()) && (k > 0)) {
+                
+                appended_sum += ((nums[nums.size()-1]+1) + (nums[nums.size()-1]+k))*(long)k/2;
+                break;
+
+            }
+
+        }
+        
+        return appended_sum;
+
+    }
+};
