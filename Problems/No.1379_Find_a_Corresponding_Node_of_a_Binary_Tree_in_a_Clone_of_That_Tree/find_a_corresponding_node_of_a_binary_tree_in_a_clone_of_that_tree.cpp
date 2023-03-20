750de770ef19492c17fbafe54567d9971455d9c9
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-20 12:47:50 +0800

    Update find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.cpp

--- Problems/No.1379_Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.cpp
+++ Problems/No.1379_Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.cpp
@@ -13,19 +13,25 @@ public:
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
         
         // parameters
-        std::queue<TreeNode*> Q;
-        TreeNode *curr = NULL;
+        std::queue<TreeNode*> Q, Q_clone;
+        TreeNode *curr = NULL, *curr_clone = NULL;
 
         // start computing
-        Q.push(cloned);
+        Q.push(original);
+        Q_clone.push(cloned);
         while (!Q.empty()) {
 
             curr = Q.front();
             Q.pop();
-            if (curr->val == target->val) return curr;
+            curr_clone = Q_clone.front();
+            Q_clone.pop();
+            if (curr == target) return curr_clone;
 
             if (curr->left) Q.push(curr->left);
             if (curr->right) Q.push(curr->right);
+
+            if (curr_clone->left) Q_clone.push(curr_clone->left);
+            if (curr_clone->right) Q_clone.push(curr_clone->right);
 
         }
 
