9337da03643cb1158278b83cde06a85739e875f4
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-20 12:44:26 +0800

    Create find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.cpp

---
+++ Problems/No.1379_Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        
+        // parameter
+        std::queue<TreeNode*> Q;
+        TreeNode *curr = NULL;
+
+        // start computing
+        Q.push(cloned);
+        while (!Q.empty()) {
+
+            curr = Q.front();
+            Q.pop();
+            if (curr->val == target->val) return curr;
+
+            if (curr->left) Q.push(curr->left);
+            if (curr->right) Q.push(curr->right);
+
+        }
+
+        return NULL;
+
+    }
+};
