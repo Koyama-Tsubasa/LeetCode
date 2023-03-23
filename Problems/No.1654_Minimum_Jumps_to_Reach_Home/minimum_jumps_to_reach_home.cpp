a1eb1b5d36181c3938fbcc70ce8f6a26571c9280
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-23 13:56:48 +0800

    Update minimum_jumps_to_reach_home.cpp

--- Problems/No.1654_Minimum_Jumps_to_Reach_Home/minimum_jumps_to_reach_home.cpp
+++ Problems/No.1654_Minimum_Jumps_to_Reach_Home/minimum_jumps_to_reach_home.cpp
@@ -5,29 +5,29 @@ public:
         // parameters
         int jumps = -1;
         std::unordered_set<int> forbiddenPositions(forbidden.begin(), forbidden.end());
-        std::queue<std::pair<int, bool>> possiblePositions;
+        std::queue<std::pair<int, bool>> possibleJumps;
 
         // start computing
-        possiblePositions.push({0, true});
-        while (!possiblePositions.empty()) {
+        possibleJumps.push({0, true});
+        while (!possibleJumps.empty()) {
 
             jumps++;
-            auto pP_size = possiblePositions.size();
+            auto pP_size = possibleJumps.size();
 
             for (int i=0; i<pP_size; i++) {
 
-                auto [currP, backC] = possiblePositions.front();
-                possiblePositions.pop();
+                auto [currP, backC] = possibleJumps.front();
+                possibleJumps.pop();
 
                 if (currP == x) return jumps;
                 else if (forbiddenPositions.find(currP) != forbiddenPositions.end()) continue;
                 else {
 
                     // backward (check if it backwarded once, also for the negative position)
-                    if (backC && (0 <= currP-b)) possiblePositions.push({currP-b, false});
+                    if (backC && (0 <= currP-b)) possibleJumps.push({currP-b, false});
 
                     // forward (check if it is going to jump redundandly)
-                    if (currP-b <= 2000) possiblePositions.push({currP+a, true});
+                    if (currP-b <= 2000) possibleJumps.push({currP+a, true});
 
                 }
 
