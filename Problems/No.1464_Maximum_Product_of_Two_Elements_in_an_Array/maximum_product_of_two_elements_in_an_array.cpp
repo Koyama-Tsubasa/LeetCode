ae03b637d1f6f00c82560923fd8774a3ccc77160
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-27 13:11:16 +0800

    Update maximum_product_of_two_elements_in_an_array.cpp

--- Problems/No.1464_Maximum_Product_of_Two_Elements_in_an_Array/maximum_product_of_two_elements_in_an_array.cpp
+++ Problems/No.1464_Maximum_Product_of_Two_Elements_in_an_Array/maximum_product_of_two_elements_in_an_array.cpp
@@ -3,7 +3,7 @@ public:
     int maxProduct(vector<int>& nums) {
         
         // start computing
-        std::sort(nums.begin(), nums.end(), greater<int>());
+        std::sort(nums.begin(), nums.end(), std::greater<int>());
 
         return ((nums[0]-1) * (nums[1]-1));
 
