5f00682e00e1b6c66f2ecc02baf0bacff24ef228
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-28 15:41:30 +0800

    Create find_subsequence_of_length_k_with_the_largest_sum.cpp

---
+++ Problems/No.2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum/find_subsequence_of_length_k_with_the_largest_sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        
+        // parameter
+        std::vector<int> MS_Sequence;
+        std::priority_queue<int> IncreasingOrder;
+        std::map<int, int> LargestSum;
+
+        // start computing
+        // create MAX-Heap
+        for (auto &num: nums) IncreasingOrder.push(num);
+
+        // find largest sum
+        for (int i=0; i<k; i++) {
+
+            LargestSum[IncreasingOrder.top()]++;
+            IncreasingOrder.pop();
+
+        }
+
+        // push in original order
+        for (auto &num: nums) {
+            if (LargestSum[num] > 0) {
+
+                MS_Sequence.push_back(num);
+                LargestSum[num]--;
+
+            }
+        }
+
+        return MS_Sequence;
+
+    }
+};
