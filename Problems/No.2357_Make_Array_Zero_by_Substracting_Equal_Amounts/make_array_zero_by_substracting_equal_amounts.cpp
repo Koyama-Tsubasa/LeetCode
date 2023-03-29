be2e2f8a25172c3b777e9d2c2a37fa5ba1c49893
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-03-29 09:50:55 +0800

    Create make_array_zero_by_substracting_equal_amounts.cpp

---
+++ Problems/No.2357_Make_Array_Zero_by_Substracting_Equal_Amounts/make_array_zero_by_substracting_equal_amounts.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        
+        // parameters
+        std::unordered_set<int> numSet;
+
+        // start computing
+        for (auto &num: nums) 
+            if ((num > 0) && (numSet.count(num) == 0)) 
+                numSet.insert(num);
+        
+        return numSet.size();
+
+    }
+};
