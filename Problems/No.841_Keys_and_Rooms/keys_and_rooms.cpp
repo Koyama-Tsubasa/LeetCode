32f11eb0095fc8a4fe8b8e41c75a8dd09741bf03
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-04-05 17:08:40 +0800

    Create keys_and_rooms.cpp

---
+++ Problems/No.841_Keys_and_Rooms/keys_and_rooms.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        
+        // parameters
+        int room_num = rooms.size(), room_count = 0;
+        std::vector<bool> visited(room_num, false);
+        std::queue<int> keys;
+
+        // start computing
+        keys.push(0);
+        while (!keys.empty()) {
+
+            auto key = keys.front();
+            keys.pop();
+            
+            if (visited[key] == true) continue;
+            else {
+
+                room_count++;
+                visited[key] = true;
+                for (auto find_key: rooms[key]) 
+                    if (visited[find_key] == false) keys.push(find_key);
+
+            }
+
+        }
+
+        return (room_count == room_num);
+
+    }
+};
