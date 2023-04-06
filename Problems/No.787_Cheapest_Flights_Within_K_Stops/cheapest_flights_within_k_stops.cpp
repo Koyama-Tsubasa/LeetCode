66fe4cd0155fea7103a5bee28b2d5afc42695751
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-04-06 15:54:42 +0800

    Create cheapest_flights_within_k_stops.cpp

---
+++ Problems/No.787_Cheapest_Flights_Within_K_Stops/cheapest_flights_within_k_stops.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        // parameters
+        std::vector<std::vector<std::pair<int, int>>> flight_graph(n);
+        std::queue<std::pair<int, int>> flight;
+        std::vector<int> price_to_dest(n, INT_MAX);
+
+        // start computing
+        // construct graph information
+        for (auto ticket: flights) {
+
+            auto s = ticket[0], d = ticket[1], cost = ticket[2];
+            flight_graph[s].push_back({d, cost});
+
+        }
+
+        // start visiting K stops using BFS & Dijkstra
+        flight.push({src, 0});
+        price_to_dest[src] = 0;
+        for (int i=0; i<=k; i++) {
+
+            int flight_count = flight.size();
+            for (int j=0; j<flight_count; j++) {
+
+                auto [dest, cost] = flight.front();
+                flight.pop();
+                
+                for (auto [next_dst, added_price]: flight_graph[dest]) {
+                    if ((cost + added_price) < price_to_dest[next_dst]) {
+
+                        price_to_dest[next_dst] = cost + added_price;
+                        flight.push({next_dst, price_to_dest[next_dst]});
+
+                    }
+                }
+
+            }
+
+        }
+
+        return (price_to_dest[dst] != INT_MAX) ? price_to_dest[dst] : -1;
+
+    }
+};
