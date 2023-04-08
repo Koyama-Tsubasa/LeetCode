eef6f2e59ad85319efeaff907f8907336abca094
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-04-08 21:56:11 +0800

    Create minimum_cost_to_make_at_least_one_valid_path_in_a_grid.cpp

---
+++ Problems/No.1368_Minimum_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid/minimum_cost_to_make_at_least_one_valid_path_in_a_grid.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+        
+        // reference:
+        // https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/solutions/524936/c-0-1-bfs-o-n-instead-of-o-nlogn/
+
+        // parameters
+        int final_cost = 0;
+        int m = grid.size(), n = grid[0].size();
+        std::vector<std::pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        std::vector<bool> visited(m*n, false);
+        std::deque<std::pair<int, int>> next_grid_with_less_cost;
+
+        // start computing
+        next_grid_with_less_cost.push_back({0, 0});
+        while (!next_grid_with_less_cost.empty()) {
+
+            auto [curr_grid, curr_cost] = next_grid_with_less_cost.front();
+            next_grid_with_less_cost.pop_front();
+
+            int curr_grid_r = curr_grid / n;
+            int curr_grid_c = curr_grid % n;
+            if (visited[curr_grid] == false) {
+
+                visited[curr_grid] = true;
+                final_cost = curr_cost;
+
+            }
+            
+            if ((curr_grid_r == m-1) && (curr_grid_c == n-1)) break;
+            else {
+
+                for (auto &[vr, vc]: dirs) {
+
+                    int next_r = curr_grid_r + vr;
+                    int next_c = curr_grid_c + vc;
+                    int next_grid = next_r*n + next_c;
+                    if ((next_r < 0) || (next_r >= m) || (next_c < 0) || (next_c >= n) || 
+                        (visited[next_grid] == true)) continue;
+                    
+                    bool add_cost = false;
+                    if (!(((grid[curr_grid_r][curr_grid_c] == 1) && (vr == 0) && (vc == 1)) ||
+                          ((grid[curr_grid_r][curr_grid_c] == 2) && (vr == 0) && (vc == -1)) ||
+                          ((grid[curr_grid_r][curr_grid_c] == 3) && (vr == 1) && (vc == 0)) ||
+                          ((grid[curr_grid_r][curr_grid_c] == 4) && (vr == -1) && (vc == 0))))
+                        add_cost = true;
+                    
+                    if (add_cost == false) next_grid_with_less_cost.push_front({next_grid, curr_cost});
+                    else next_grid_with_less_cost.push_back({next_grid, curr_cost + 1});
+
+                }
+
+            }
+
+        }
+
+        return final_cost;
+
+    }
+};
