3040df4a16b9022ba801fc0f89d54cd54a9b17d2
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-04-10 14:11:22 +0800

    Create left_and_right_sum_differences.cpp

---
+++ Problems/No.2574_Left_and_Right_Sum_Differences/left_and_right_sum_differences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> leftRigthDifference(vector<int>& nums) {
+        
+        // parameter
+        std::vector<int> LSum(nums.size()+1, 0);
+        std::vector<int> LRDifference;
+
+        // start computing
+        std::partial_sum(nums.begin(), nums.end(), LSum.begin()+1);
+        for (int i=0; i<nums.size(); i++) 
+            LRDifference.push_back(std::abs(LSum.back() - LSum[i+1] - LSum[i]));
+
+        return LRDifference;
+
+    }
+};
