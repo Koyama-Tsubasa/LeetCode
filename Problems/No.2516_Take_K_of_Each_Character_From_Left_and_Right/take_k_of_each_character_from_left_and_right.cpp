34b8d290eb751083e46d080f5bd24220cefe4874
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-04-20 17:27:03 +0800

    Create take_k_of_each_character_from_left_and_right.cpp

---
+++ Problems/No.2516_Take_K_of_Each_Character_From_Left_and_Right/take_k_of_each_character_from_left_and_right.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int takeCharacters(string s, int k) {
+        
+        // parameters
+        int minutes = INT_MAX;
+        std::unordered_map<char, int> char_map;
+
+        // start computing
+        // calculate the total number of each character and check for the constraint
+        for (auto &c: s) char_map[c]++;
+        if ((char_map['a'] < k) || (char_map['b'] < k) || (char_map['c'] < k)) return -1;
+
+        // traverse the string from left and right to find the minimum minutes
+        int s_len = s.length();
+        for (int left=0, right=0; left<s_len; left++) {
+
+            while ((right <= s_len) && 
+                   (char_map['a'] >= k) && (char_map['b'] >= k) && (char_map['c'] >= k)) {
+                
+                minutes = std::min(minutes, left + (s_len-right));
+                if (right < s_len) char_map[s[right]]--;
+                right++;
+
+            }
+            char_map[s[left]]++;
+
+        }
+        
+        return minutes;
+
+    }
+};
