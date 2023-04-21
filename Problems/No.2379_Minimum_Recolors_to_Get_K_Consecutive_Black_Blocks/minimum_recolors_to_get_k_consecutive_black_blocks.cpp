ea67679ef34d12c06358fc2a1dca211e69445a8d
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-04-21 14:09:49 +0800

    Create minimum_recolors_to_get_k_consecutive_black_blocks.cpp

---
+++ Problems/No.2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks/minimum_recolors_to_get_k_consecutive_black_blocks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        
+        // parameters
+        int min_changes = 0;
+        int num_of_w = 0;
+
+        // start computing
+        for (int i=0; i<k; i++) if (blocks[i] == 'W') num_of_w++;
+        min_changes = num_of_w;
+        for (int i=1; i<blocks.size()-k+1; i++) {
+
+            if (blocks[i-1] == 'W') num_of_w--;
+            if (blocks[i+k-1] == 'W') num_of_w++;
+            min_changes = std::min(min_changes, num_of_w);
+
+        }
+
+        return min_changes;
+
+    }
+};
