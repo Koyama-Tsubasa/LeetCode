e28492de1db01cc1029b59dfc08ffd0342070b49
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-04-21 14:58:10 +0800

    Create max_consecutive_ones_III.cpp

---
+++ Problems/No.1004_Max_Consecutive_Ones_III/max_consecutive_ones_III.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        
+        // parameter
+        int max_ones = 0;
+        int start_idx = 0;
+
+        // start computing
+        for (int end_idx=0; end_idx<nums.size(); end_idx++) {
+
+            if (nums[end_idx] == 0) k--;
+            while (k < 0) if (nums[start_idx++] == 0) k++;
+            max_ones = std::max(max_ones, end_idx - start_idx + 1);
+
+        }
+
+        return max_ones;
+
+    }
+};
