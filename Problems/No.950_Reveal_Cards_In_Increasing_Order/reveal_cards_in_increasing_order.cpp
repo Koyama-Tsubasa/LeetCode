fae15c5bff879a48f1b56882a4e373a3d6c8b9e2
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-04-26 14:06:43 +0800

    Create reveal_cards_in_increasing_order.cpp

---
+++ Problems/No.950_Reveal_Cards_In_Increasing_Order/reveal_cards_in_increasing_order.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        
+        // parameter
+        std::vector<int> ReorderedDeck(deck.size(), 0);
+        std::queue<int> increasing_deck_idx;
+
+        // start computing
+        std::sort(deck.begin(), deck.end());
+        for (int i=0; i<deck.size(); i++) increasing_deck_idx.push(i);
+
+        for (auto &card: deck) {
+
+            ReorderedDeck[increasing_deck_idx.front()] = card;
+            increasing_deck_idx.pop();
+            increasing_deck_idx.push(increasing_deck_idx.front());
+            increasing_deck_idx.pop();
+
+        }
+
+        return ReorderedDeck;
+
+    }
+};
