2e19111c7768d1adcb1d75f82037a90008779665
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-05-03 12:28:34 +0800

    Create count_good_triplets.cpp

---
+++ Problems/No.1534_Count_Good_Triplets/count_good_triplets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+
+        // parameter
+        int cnt = 0;
+        int arr_size = arr.size();
+
+        // start computing
+        for (int i=0; i<arr_size-2; i++)
+            for (int j=i+1; j<arr_size-1; j++)
+                for (int k=j+1; k<arr_size; k++)
+                    if ((abs(arr[i]-arr[j]) <= a) &&
+                        (abs(arr[j]-arr[k]) <= b) &&
+                        (abs(arr[i]-arr[k]) <= c))
+                        cnt++;
+        
+        return cnt;
+
+    }
+};
