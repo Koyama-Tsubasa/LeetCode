14360bba6657b1f0d86590af49e6b6c7b916deb2
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-05-03 12:32:39 +0800

    Update count_good_triplets.cpp

--- Problems/No.1534_Count_Good_Triplets/count_good_triplets.cpp
+++ Problems/No.1534_Count_Good_Triplets/count_good_triplets.cpp
@@ -9,11 +9,11 @@ public:
         // start computing
         for (int i=0; i<arr_size-2; i++)
             for (int j=i+1; j<arr_size-1; j++)
-                for (int k=j+1; k<arr_size; k++)
-                    if ((abs(arr[i]-arr[j]) <= a) &&
-                        (abs(arr[j]-arr[k]) <= b) &&
-                        (abs(arr[i]-arr[k]) <= c))
-                        cnt++;
+                if (abs(arr[i]-arr[j]) <= a)
+                    for (int k=j+1; k<arr_size; k++)
+                        if ((abs(arr[j]-arr[k]) <= b) &&
+                            (abs(arr[i]-arr[k]) <= c))
+                            cnt++;
         
         return cnt;
 
