43bf7373d32e744d14c0cf2c4db39e327e4ce344
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-05-05 12:53:10 +0800

    Create detect_pattern_of_length_m_repeated_k_or_more_times.cpp

---
+++ Problems/No.1566_Detect_Pattern_of_length_M_Repeated_K_or_More_Times/detect_pattern_of_length_m_repeated_k_or_more_times.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsPattern(vector<int>& arr, int m, int k) {
+        
+        // parameters
+        int pattern_cnt = 0;
+
+        // start computing
+        if (arr.size() < m*k) return false;
+        for (int i=0; i<arr.size()-m; i++) {
+
+            if (arr[i] == arr[i+m]) pattern_cnt++;
+            else pattern_cnt = 0;
+
+            if (pattern_cnt == m*(k-1)) return true;
+
+        }
+
+        return false;
+
+    }
+};
