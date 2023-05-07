12baaa3346612682136cd0d4969e0225e0389d82
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-05-07 14:58:50 +0800

    Update number_of_ways_to_buy_pens_and_pencils.cpp

--- Problems/No.2240_Number_of_Ways_to_Buy_Pens_and_Pencils/number_of_ways_to_buy_pens_and_pencils.cpp
+++ Problems/No.2240_Number_of_Ways_to_Buy_Pens_and_Pencils/number_of_ways_to_buy_pens_and_pencils.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long waysToBuyPensPencils(int total, int cost1, int cost2) {
         
-        // parameter
+        // parameters
         long long cnt = 0;
         int higher_cost = std::max(cost1, cost2);
         int lower_cost = std::min(cost1, cost2);
