f2e19a66082a51181fdce162cf834c75d70367d6
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-05-13 22:47:28 +0800

    Update number_of_longest_increasing_subsequence.cpp

--- Problems/No.673_Number_of_Longest_Increasing_Subsequence/number_of_longest_increasing_subsequence.cpp
+++ Problems/No.673_Number_of_Longest_Increasing_Subsequence/number_of_longest_increasing_subsequence.cpp
@@ -3,7 +3,7 @@ public:
     int findNumberOfLIS(vector<int>& nums) {
         
         // parameters
-        std::vector<int> DP(nums.size(), 1);
+        std::vector<int> LIS_len(nums.size(), 1);
         std::vector<int> LIS_cnt(nums.size(), 1);
         int MAX_LIS = 1;
         int MAX_LIS_cnt = 0;
@@ -13,20 +13,20 @@ public:
 
             for (int j=0; j<i; j++) 
                 if (nums[j] < nums[i]) 
-                    if (DP[i] < (DP[j] + 1)) {
+                    if (LIS_len[i] < (LIS_len[j] + 1)) {
 
-                        DP[i] = DP[j] + 1;
+                        LIS_len[i] = LIS_len[j] + 1;
                         LIS_cnt[i] = LIS_cnt[j];
 
                     }
-                    else if ((DP[j] + 1) == DP[i]) LIS_cnt[i] += LIS_cnt[j];
+                    else if ((LIS_len[j] + 1) == LIS_len[i]) LIS_cnt[i] += LIS_cnt[j];
             
-            MAX_LIS = std::max(MAX_LIS, DP[i]);
+            MAX_LIS = std::max(MAX_LIS, LIS_len[i]);
 
         }
 
         for (int idx=0; idx<nums.size(); idx++)
-            if (DP[idx] == MAX_LIS) MAX_LIS_cnt += LIS_cnt[idx];
+            if (LIS_len[idx] == MAX_LIS) MAX_LIS_cnt += LIS_cnt[idx];
 
         return MAX_LIS_cnt;
 
