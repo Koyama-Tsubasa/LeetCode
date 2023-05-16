bdffdbf0e96cdcba19b27c0301554e70a26c6ff6
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-05-16 15:52:15 +0800

    Create repeated_dna_sequences.cpp

---
+++ Problems/No.187_Repeated_DNA_Sequences/repeated_dna_sequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        
+        // parameter
+        std::vector<std::string> RepeatedDNA;
+        std::unordered_map<std::string, int> DNA_cnt;
+
+        // start computing
+        if (s.length() <= 10) return {};
+        for (int i=0; i<=s.length()-10; i++) {
+
+            std::string sub_DNA = s.substr(i, 10);
+            DNA_cnt[sub_DNA]++;
+
+        }
+        for (auto &[sDNA, cnt]: DNA_cnt)
+            if (cnt > 1) RepeatedDNA.push_back(sDNA);
+        
+        return RepeatedDNA;
+
+    }
+};
