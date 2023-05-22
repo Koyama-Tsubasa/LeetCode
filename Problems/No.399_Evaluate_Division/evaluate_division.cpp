be465a333087899e54373651aa99ecc8dc1e70b0
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-05-22 14:15:18 +0800

    Create evaluate_division.cpp

---
+++ Problems/No.399_Evaluate_Division/evaluate_division.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    // find the answer for each equation using DFS
+    double DFS(double val, std::string node, std::string dest, std::set<string> &visited,
+               std::unordered_map<std::string, std::unordered_map<std::string, double>> &Graph) {
+
+        // check if visit the destination
+        if ((node == dest) && (Graph.find(node) != Graph.end())) return val;
+        else {
+
+            double temp;
+            for (auto &[next_node, cost]: Graph[node]) 
+                if (visited.count(next_node) == 0) {
+
+                    visited.insert(next_node);
+                    temp = DFS(val*cost, next_node, dest, visited, Graph);
+                    if (temp != -1.0) return temp;
+
+                }
+            
+            return -1.0;
+
+        }
+
+    }
+
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        
+        // parameters
+        std::vector<double> Equations;
+
+        // start computing
+        // build Equation Graph
+        std::unordered_map<std::string, std::unordered_map<std::string, double>> Graph;
+        for (int i=0; i<equations.size(); i++) {
+
+            Graph[equations[i][0]][equations[i][1]] = values[i];
+            Graph[equations[i][1]][equations[i][0]] = 1/values[i];
+
+        }
+
+        // start compute for each equation using DFS
+        std::set<std::string> visited;
+        for (auto &equation: queries) {
+
+            visited.clear();
+            Equations.push_back(DFS(1, equation[0], equation[1], visited, Graph));
+
+        }
+
+        return Equations;
+
+    }
+};
